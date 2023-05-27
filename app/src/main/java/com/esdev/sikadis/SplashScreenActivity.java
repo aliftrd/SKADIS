f5b1312896c232af63bea974ae176189cc14b47d
Irfan Maulana Ahmad Ghani <irfanbataan110@gmail.com>
2023-05-27 20:51:09 +0700

    Get API LayananFragment and Fixs SplashScreenActivity

--- app/src/main/java/com/esdev/sikadis/SplashScreenActivity.java
+++ app/src/main/java/com/esdev/sikadis/SplashScreenActivity.java
@@ -1,25 +1,60 @@
 package com.esdev.sikadis;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000; // Waktu tampilan splash screen (dalam milidetik)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
+        // Memeriksa koneksi internet
+        if (isNetworkAvailable()) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        } else {
+            showNoInternetDialog();
+        }
+    }
+
+    // Metode untuk memeriksa koneksi internet
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
+        return networkCapabilities != null && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET);
+    }
+
+    // Metode untuk menampilkan dialog jika tidak ada koneksi internet
+    private void showNoInternetDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Tidak Ada Koneksi Internet");
+        builder.setMessage("Silakan periksa koneksi internet Anda dan coba lagi.");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Tutup", new DialogInterface.OnClickListener() {
             @Override
-            public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-                startActivity(intent);
+            public void onClick(DialogInterface dialogInterface, int i) {
                 finish();
             }
-        }, 3000);
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
